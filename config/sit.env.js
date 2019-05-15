786dbfc6f934694538e6f01648a211d1d6dbccbc
vangyong <vangyong@126.com>
2019-05-15 21:04:41 +0800

    修改

--- config/sit.env.js
+++ config/sit.env.js
@@ -2,5 +2,5 @@ module.exports = {
   NODE_ENV: '"production"',
   ENV_CONFIG: '"sit"',
   //BASE_API: '"https://api-sit"'
-  //BASE_API: '"http://localhost:11000"'
+  BASE_API: '"http://localhost:8000"'
 }
