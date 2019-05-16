6642c5fcab303bf86275215ab29feaaa5a2433ba
wangyong <vangyong@126.com>
2019-05-16 15:47:26 +0800

    xiugai

--- config/index.js
+++ config/index.js
@@ -6,7 +6,7 @@ const path = require('path')
 
 module.exports = {
   dev: {
-    env: require('./dev.env'),
+    //env: require('./dev.env'),
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/system',
